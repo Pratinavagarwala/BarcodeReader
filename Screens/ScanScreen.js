79fed7ad7b0d7ee2c8f4ee4b50231af6e006e708
Pratinavagarwala <66483787+Pratinavagarwala@users.noreply.github.com>
2020-12-08 12:06:46 +0530

    Hi

---
+++ Screens/ScanScreen.js
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import {View ,Text , StyleSheet,TouchableOpacity,Image} from 'react-native';
+import * as Permissions from 'expo-permissions';
+import {BarCodeScanner} from 'expo-barcode-scanner';
+
+export default class ScanScreen extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            hasCameraPermissions:null,
+            scanned:false,
+            scannedData:"",
+            buttonState:"normal",
+        }
+    }
+     getCameraPermission=async()=>{
+        const {status}=await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({
+            hasCameraPermissions:status==="granted",
+            buttonState:"clicked"
+        })
+    }
+    handleBarcodeScanner=async({type,data})=>{
+        this.setState({
+            scanned:true,
+            scannedData:data,
+            buttonState:"normal",
+
+        })
+    }
+    render(){
+        const buttonState=this.state.buttonState
+        if(buttonState==="clicked"&& hasCameraPermissions){
+            return(
+                <BarCodeScanner onBarCodeScanned={this.state.scanned===true?undefined:this.handleBarcodeScanner } 
+                style={StyleSheet.absoluteFillObject}>
+                </BarCodeScanner>
+            )
+        }
+        else if(buttonState==="normal"){
+
+        
+        return(
+            <View style={Styles.container}>
+                <Text>Scan Screen </Text>
+                <Text style={{
+                    marginTop:50
+                }}>{this.state.hasCameraPermissions===true?this.state.scannedData:"Request Camera Permission"}</Text>
+                <TouchableOpacity style={{marginTop:50,alignItems:"center",alignSelf:"center"}}>
+                <Text> Scan QR-CODE</Text> 
+                </TouchableOpacity>
+                <Image/>
+            </View>
+        );}
+    }
+}
+
+var Styles=StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+    }
+})
