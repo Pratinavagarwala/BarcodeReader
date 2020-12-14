eafda13a2ab590e503aeec7b5bb4fc4659eefa2e
Pratinavagarwala <66483787+Pratinavagarwala@users.noreply.github.com>
2020-12-14 10:38:26 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {ScanScreen} from "./Screens";
+import ScanScreen from "./Screens/ScanScreen";
 
 export default function App() {
   return (
